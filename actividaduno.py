b84a388b11275940144ee0826cf691ce6e8d7ca2
juanchinetti <jichinetti@gmail.com>
2024-05-02 19:14:48 -0300

    primer commit

---
+++ actividaduno.py
@@ -0,0 +1,12 @@
+#Ejercicio 1: Calculadora de Promedio
+
+# Solicitar al usuario que ingrese tres números
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+num3 = float(input("Ingrese el tercer número: "))
+
+# Calcular el promedio de los números
+promedio = (num1 + num2 + num3) / 3
+
+# Mostrar el resultado
+print("El promedio de los números ingresados es:", promedio)
