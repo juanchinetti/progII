314c71e5b6583b678605c307fe0d7b71b91fb813
juanchinetti <jichinetti@gmail.com>
2024-05-02 19:32:19 -0300

    tercer commit

---
+++ actividadtres.py
@@ -0,0 +1,11 @@
+#Ejercicio 3: Área de un Triángulo
+
+# Solicitar al usuario que ingrese la base y la altura del triángulo
+base = float(input("Ingrese la longitud de la base del triángulo: "))
+altura = float(input("Ingrese la altura del triángulo: "))
+
+# Calcular el área del triángulo
+area = (base * altura) / 2
+
+# Mostrar el área del triángulo
+print("El área del triángulo es:", area)
