170a47375ca364fa8109283e777a033635240f37
juanchinetti <jichinetti@gmail.com>
2024-06-11 21:43:48 -0300

    _main_.py

---
+++ trabajo-practico-de-programacion/_main_.py
@@ -0,0 +1,13 @@
+from producto import Producto
+from electronico import Electronico
+from alimento import Alimento
+
+# Crear instancias de las clases hijas
+producto_electronico = Electronico("Laptop", 1500.00, 10, "Lenovo", "ThinkPad X1")
+producto_alimento = Alimento("Manzanas", 2.50, 100, "2024-06-30")
+
+# Mostrar información de los productos
+print("Información del producto electrónico:")
+producto_electronico.mostrar_informacion()
+print("\nInformación del producto alimenticio:")
+producto_alimento.mostrar_informacion()
