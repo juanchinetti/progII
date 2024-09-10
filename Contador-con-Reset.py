e398b90d9d531c041dbdcdf11ec6f8915f4e3f65
juanchinetti <jichinetti@gmail.com>
2024-09-10 06:46:39 -0300

    contador-con-reset

--- Contador-con-Reset.py
+++ Contador-con-Reset.py
@@ -13,18 +13,18 @@ def decrementar():
 def resetear():
     contador_var.set(0)
 
-# Configuración de la ventana principal
+
 ventana = tk.Tk()
 ventana.title("Contador")
 
-# Etiqueta
+
 tk.Label(ventana, text="Contador").pack()
 
-# LineEdit no editable
+
 contador_var = tk.StringVar(value="0")
 tk.Entry(ventana, textvariable=contador_var, state="readonly").pack()
 
-# Botones
+
 tk.Button(ventana, text="suma", command=incrementar).pack()
 tk.Button(ventana, text="resta", command=decrementar).pack()
 tk.Button(ventana, text="Reset", command=resetear).pack()
