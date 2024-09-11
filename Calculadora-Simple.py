f09df291b362b6f77470e4d644165f1faed67105
juanchinetti <jichinetti@gmail.com>
2024-09-11 10:23:57 -0300

    Calculadora-Simple

--- Calculadora-Simple.py
+++ Calculadora-Simple.py
@@ -1,5 +1,5 @@
 #Ejercicio 2 – Calculadora Simple
-#Esta aplicación es una calculadora básica que permite realizar operaciones aritméticas simples.
+
 
 
 import tkinter as tk
@@ -16,27 +16,27 @@ def calcular(operacion):
     elif operacion == "/":
         resultado_var.set(num1 / num2)
 
-# Configuración de la ventana principal
+
 ventana = tk.Tk()
 ventana.title("Calculadora Simple")
 
-# Entrada para el primer número
+
 tk.Label(ventana, text="Número 1").grid(row=0, column=0)
 entrada1 = tk.Entry(ventana)
 entrada1.grid(row=0, column=1)
 
-# Entrada para el segundo número
+
 tk.Label(ventana, text="Número 2").grid(row=1, column=0)
 entrada2 = tk.Entry(ventana)
 entrada2.grid(row=1, column=1)
 
-# Botones de operaciones
+
 tk.Button(ventana, text="+", command=lambda: calcular("+")).grid(row=2, column=0)
 tk.Button(ventana, text="-", command=lambda: calcular("-")).grid(row=2, column=1)
 tk.Button(ventana, text="", command=lambda: calcular("")).grid(row=3, column=0)
 tk.Button(ventana, text="/", command=lambda: calcular("/")).grid(row=3, column=1)
 
-# Resultado
+
 tk.Label(ventana, text="Resultado").grid(row=4, column=0)
 resultado_var = tk.StringVar()
 tk.Entry(ventana, textvariable=resultado_var, state="readonly").grid(row=4, column=1)
