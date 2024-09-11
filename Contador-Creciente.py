94d825554d74c8c23f25d0314de40050f10a7320
juanchinetti <jichinetti@gmail.com>
2024-09-11 10:17:11 -0300

    contador-creciente

--- Contador-Creciente.py
+++ Contador-Creciente.py
@@ -1,7 +1,5 @@
 #Ejercicio 1.1 - Contador Creciente
 
-#Este programa incrementa un contador cada vez que se presiona el botón "+".
-
 
 import tkinter as tk
 
@@ -9,18 +7,18 @@ def incrementar():
     valor = int(contador_var.get())
     contador_var.set(valor + 1)
 
-# Configuración de la ventana principal
+
 ventana = tk.Tk()
 ventana.title("Contador Creciente")
 
-# Etiqueta
+
 tk.Label(ventana, text="Contador").pack()
 
-# LineEdit no editable
+
 contador_var = tk.StringVar(value="0")
 tk.Entry(ventana, textvariable=contador_var, state="readonly").pack()
 
-# Botón "+"
+
 tk.Button(ventana, text="+", command=incrementar).pack()
 
 ventana.mainloop()
