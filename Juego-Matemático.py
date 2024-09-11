76ae354ad38027b2fcf6d111e2198ffe91a893ad
juanchinetti <jichinetti@gmail.com>
2024-09-11 10:21:29 -0300

    Juego-Matemático

--- Juego-Matemático.py
+++ Juego-Matemático.py
@@ -1,5 +1,4 @@
 #Ejercicio 4 – Juego Matemático
-#Este juego muestra dos números y una operación, y permite al usuario ingresar su respuesta para verificar si es correcta o incorrecta.
 
 import tkinter as tk
 import random
@@ -30,30 +29,30 @@ def verificar_respuesta():
     else:
         resultado_var.set("Incorrecto!")
 
-# Configuración de la ventana principal
+
 ventana = tk.Tk()
 ventana.title("Juego Matemático")
 
-# Mostrar la operación
+
 operacion_var = tk.StringVar()
 tk.Label(ventana, textvariable=operacion_var).grid(row=0, columnspan=2)
 
-# Entrada para la respuesta
+
 tk.Label(ventana, text="Tu Respuesta:").grid(row=1, column=0)
 respuesta_var = tk.Entry(ventana)
 respuesta_var.grid(row=1, column=1)
 
-# Botón para verificar
+
 tk.Button(ventana, text="Verificar", command=verificar_respuesta).grid(row=2, columnspan=2)
 
-# Mostrar el resultado
+
 resultado_var = tk.StringVar()
 tk.Label(ventana, textvariable=resultado_var).grid(row=3, columnspan=2)
 
-# Almacenar el resultado correcto (no visible)
+
 resultado_esperado_var = tk.StringVar()
 
-# Generar la primera operación
+
 generar_operacion()
 
 ventana.mainloop()
