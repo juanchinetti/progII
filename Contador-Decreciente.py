5debadde7cd8b94d180b680ed5144e0d6a23d7d4
juanchinetti <jichinetti@gmail.com>
2024-09-11 10:16:09 -0300

    contador-decreciente

--- Contador-Decreciente.py
+++ Contador-Decreciente.py
@@ -1,6 +1,6 @@
 #Ejercicio 1.2 - Contador Decreciente
 
-#Este programa decrementa un contador cada vez que se presiona el botón "-". El contador empieza en 88.
+
 
 import tkinter as tk
 
@@ -8,18 +8,18 @@ def decrementar():
     valor = int(contador_var.get())
     contador_var.set(valor - 1)
 
-# Configuración de la ventana principal
+
 ventana = tk.Tk()
 ventana.title("Contador Decreciente")
 
-# Etiqueta
+
 tk.Label(ventana, text="Contador").pack()
 
-# LineEdit no editable
-contador_var = tk.StringVar(value="88")
+
+contador_var = tk.StringVar(value="100")
 tk.Entry(ventana, textvariable=contador_var, state="readonly").pack()
 
-# Botón "-"
+
 tk.Button(ventana, text="-", command=decrementar).pack()
 
 ventana.mainloop()
